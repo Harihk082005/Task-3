708ff6f7f89ac465fae88e17e0e9f4deb752140b
Jahan221801 <162282316+Jahan221801@users.noreply.github.com>
2025-02-28 11:56:39 +0530

    Update calc.java by feature 1

--- calc.java
+++ calc.java
@@ -1,1 +1,11 @@
+public class Calc {
+    public int add(int a, int b) { return a + b; }
+    public int subtract(int a, int b) { return a - b; }
+
+    public static void main(String[] args) {
+        Calc calculator = new Calc();
+        System.out.println(calculator.add(5, 3)); 
+        System.out.println(calculator.subtract(5, 3)); 
+    }
+}
 
